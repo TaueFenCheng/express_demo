4339bd869fd1198cc446ed84142588f38b60aebe
tangjiaqiang2 <tangjiaqiang2@yy.com>
2024-10-29 23:10:14 +0800

    feat: 完善router

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,72 @@
-import { engine } from 'express-handlebars';
-const express = require('express')
-import path from 'path'
-const app = express()
-const bodyParser = require('body-parser')
-import router from './router'
+const express = require("express");
+import { engine } from "express-handlebars";
+import path from "path";
+const app = express();
+const bodyParser = require("body-parser");
+import router from "./router";
+import logger from "./log/logger";
 
-   // 设置 Handlebars 作为模板引擎
-app.engine('handlebars', engine({
-  defaultLayout: false,
-}));
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('view engine', 'handlebars');
+// 设置 Handlebars 作为模板引擎
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: false,
+  })
+);
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("view engine", "handlebars");
 
 function logErrors(err, req, res, next) {
   console.error(err.stack);
-  next(err);
+  res.status(500).send("Something broke!");
+  // next(err);
 }
 
 // 设置视图文件夹
-app.set('views', path.join(__dirname,'page'));
+app.set("views", path.join(__dirname, "page"));
 // app.set('views', './page');
 
-app.use("/public",express.static(path.join(__dirname,'public')))
+app.use("/public", express.static(path.join(__dirname, "public")));
 // app.use(bodyParser)
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(logErrors);
 // http://localhost:3000/public/image/test.png
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello worldssdasddasda')
-})
-app.use('/router',router)
-app.listen(3000)  
+app.get("/", (req, res) => {
+  res.send("hello worldssdasddasda");
+});
+
+app.post("/post", (req, res, next) => {
+  console.log(req.body);
+  logger.info("请求post");
+  res.json(req.body);
+});
+
+
+// 链式处理get post
+app
+  .route("/events")
+  .all(function (req, res, next) {
+    // runs for all HTTP verbs first
+    // think of it as route specific middleware!
+    logger.info('单一路由')
+    next()
+  })
+  .get(function (req, res, next) {
+    res.json({name:'asdasd'});
+    next()
+  })
+  .post(function (req, res, next) {
+    // maybe add a new event...
+    logger.info('单一路由post请求')
+  });
+
+app.use("/router", router);
+app.listen(3000);
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,47 @@
-const express = require('express')
-import logger from '../log/logger'
-const router = express.Router()
+const express = require("express");
+import logger from "../log/logger";
+const router = express.Router();
 
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use((req, res, next) => {
   // console.log('Time:', Date.now())
-  logger.info(`Time: ${Date.now()}`)
-  next()
-})
+  logger.info(`Time: ${Date.now()}`);
+  next();
+});
 
 // a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
-router.use('/user/:id', (req, res, next) => {
-  console.log('Request URL:', req.originalUrl)
-  next()
-}, (req, res, next) => {
-  console.log('Request Type:', req.method)
-  next()
-})
+router.use(
+  "/user/:id",
+  (req, res, next) => {
+    console.log("Request URL:", req.originalUrl);
+    next();
+  },
+  (req, res, next) => {
+    console.log("Request Type:", req.method);
+    next();
+  }
+);
 
 // a middleware sub-stack that handles GET requests to the /user/:id path
-router.get('/user/:id', (req, res, next) => {
-  // if the user ID is 0, skip to the next router
-  if (req.params.id === '3') next('route')
-  // otherwise pass control to the next middleware function in this stack
-  else next()
-}, (req, res, next) => {
-  // render a regular page
-  res.render('regular',{body:'hhhhhh'})
-})
+router.get(
+  "/user/:id",
+  (req, res, next) => {
+    // if the user ID is 0, skip to the next router
+    if (req.params.id === "3") next("route");
+    // otherwise pass control to the next middleware function in this stack
+    else next();
+  },
+  (req, res, next) => {
+    // render a regular page
+    res.render("regular", { body: "hhhhhh" });
+  }
+);
+
 
 // // handler for the /user/:id path, which renders a special page
-router.get('/user/:id', (req, res, next) => {
-  console.log(req.params.id)
-  res.render('special',{title:'special'})
-})
+router.get("/user/:id", (req, res, next) => {
+  console.log(req.params.id);
+  res.render("special", { title: "special" });
+});
 
-export default router
+export default router;
